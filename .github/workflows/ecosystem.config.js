42848c955d976b152814a10f9af61a79142be966
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-11-03 16:01:04 +0200

    Create ecosystem.config.js

---
+++ .github/workflows/ecosystem.config.js
@@ -0,0 +1,20 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [{
+    name: 'VinceHub',
+    script: 'npm',
+    args: 'start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 6014
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 6014
+    }
+  }]
+}
